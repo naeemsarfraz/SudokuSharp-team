cdc87cfe3e9943863da19a8f4134d14874f96578
nsarfraz <naeem.sarfraz@gmail.com>
2015-06-30 09:56:25 +0100

    these tests are all about extracting data from the model so best sit here

--- SodukoTests/SolveKnownPuzzleFixture.cs
+++ SodukoTests/SolveKnownPuzzleFixture.cs
@@ -11,59 +11,5 @@ namespace SodukoTests
     [TestFixture]
     public class SolveKnownPuzzleFixture
     {
-        [Test]
-        public void TestBlock()
-        {
-            BoardViewModel model = new BoardViewModel();
-            model.NewPuzzle(SampleData.StarterPuzzles[0]);
-            List<CellViewModel> cellViewModels = model.GetBlock(new Point(0,0));
-
-            Assert.IsTrue(cellViewModels[0].Number == null);
-            Assert.IsTrue(cellViewModels[1].Number == null);
-            Assert.IsTrue(cellViewModels[2].Number == 2);
-            
-        }
-
-        [Test]
-        public void Testrow()
-        {
-            BoardViewModel model = new BoardViewModel();
-            model.NewPuzzle(SampleData.StarterPuzzles[0]);
-            List<CellViewModel> cellViewModels = model.GetRow(0);
-
-            Assert.IsTrue(cellViewModels[0].Number == null);
-            Assert.IsTrue(cellViewModels[1].Number == null);
-            Assert.IsTrue(cellViewModels[2].Number == 2);
-
-            Assert.IsTrue(cellViewModels[3].Number == null);
-            Assert.IsTrue(cellViewModels[4].Number == null);
-            Assert.IsTrue(cellViewModels[5].Number == null);
-
-            Assert.IsTrue(cellViewModels[6].Number == 8);
-            Assert.IsTrue(cellViewModels[7].Number == null);
-            Assert.IsTrue(cellViewModels[8].Number == null);
-
-        }
-
-        [Test]
-        public void TestColumn()
-        {
-            BoardViewModel model = new BoardViewModel();
-            model.NewPuzzle(SampleData.StarterPuzzles[0]);
-            List<CellViewModel> cellViewModels = model.GetColumn(0);
-
-            Assert.IsTrue(cellViewModels[0].Number == null);
-            Assert.IsTrue(cellViewModels[1].Number == 1);
-            Assert.IsTrue(cellViewModels[2].Number == 3);
-
-            Assert.IsTrue(cellViewModels[3].Number == null);
-            Assert.IsTrue(cellViewModels[4].Number == null);
-            Assert.IsTrue(cellViewModels[5].Number == 6);
-
-            Assert.IsTrue(cellViewModels[6].Number == null);
-            Assert.IsTrue(cellViewModels[7].Number == null);
-            Assert.IsTrue(cellViewModels[8].Number == null);
-
-        }
     }
 }
