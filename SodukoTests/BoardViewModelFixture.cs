576014b6b8ee98f4a4ee315960a821f24c8b8205
nsarfraz <naeem.sarfraz@gmail.com>
2015-06-30 12:19:58 +0100

    test to show when only one value is an option fill the cell with that number

--- SodukoTests/BoardViewModelFixture.cs
+++ SodukoTests/BoardViewModelFixture.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using SudokuSharp.ViewModels;
+using SudokuSharp.WinApp;
 
 namespace SodukoTests
 {
